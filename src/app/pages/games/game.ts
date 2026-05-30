f94ec427f7ae4750f4295661bc608aa6a2f6199f
Matteo <clnmtt04b02m208x@studenti.unical.it>
2026-05-30 16:39:29 +0200

    Barra di ricerca

--- src/app/pages/games/game.ts
+++ src/app/pages/games/game.ts
@@ -2,17 +2,22 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { GameService } from '../../services/games.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './games.html',
   styleUrls: ['./games.css']
 })
 export class GamesComponent implements OnInit {
   
   games: any[] = [];
+  filteredGames: any[] = [];
+  searchQuery: string = '';
+  selectedGenre: string = '';
+  availableGenres: string[] = [];
 
   constructor(
     private gameService: GameService,
@@ -24,9 +29,24 @@ export class GamesComponent implements OnInit {
       next: (data: any) => {
         console.log("GIOCHI DAL BACKEND:", data);
         this.games = data;
+        this.filteredGames = data;
+        this.extractGenres();
         this.cdr.detectChanges(); 
       },
       error: (err: any) => console.error("Errore caricamento giochi:", err)
+    });
+  }
+
+  extractGenres() {
+    const genres = this.games.map(g => g.genere).filter(g => g);
+    this.availableGenres = [...new Set(genres)].sort();
+  }
+
+  filterGames() {
+    this.filteredGames = this.games.filter(game => {
+      const matchesSearch = game.title.toLowerCase().includes(this.searchQuery.toLowerCase());
+      const matchesGenre = this.selectedGenre ? game.genere === this.selectedGenre : true;
+      return matchesSearch && matchesGenre;
     });
   }
 
